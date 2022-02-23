30be2b083204be7b2843b0977d9b16f07e25cee1
HuabeiYou <huabeiyou@gmail.com>
2022-02-23 15:16:35 -0500

    init server

---
+++ src/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const { Server } = require('socket.io')
+
+const io = new Server(process.env.PORT, {
+  cors: {
+    origin: 'http://localhost:3000',
+    credentials: true
+  },
+})
+io.on('connection', (socket) => {
+  socket.on('connected', ({ session, student }) => {
+    socket.join(student)
+    io.to(student).emit('user joined', session)
+  })
+})
