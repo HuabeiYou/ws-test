3543ca5b8c91f62afbec21fea3a48b96a585b55d
HuabeiYou <huabeiyou@gmail.com>
2022-02-27 12:30:28 -0500

    implement heartbeat

---
+++ src/IServer.js
@@ -0,0 +1,92 @@
+const EventEmitter = require('events')
+const { WebSocketServer } = require('ws')
+const { v4 } = require('uuid')
+const debug = require('debug')
+const wssDebug = debug('wss')
+const clientDebug = debug('client')
+
+module.exports = class IServer extends EventEmitter {
+  constructor(props) {
+    super(props)
+    this.rooms = new Map()
+    this.clients = new Map()
+    this.connections = new Set()
+    this.wss = new WebSocketServer(props)
+    this.wss.on('connection', (socket, req) =>
+      this._onSocketConnection(socket, req)
+    )
+    this.wss.on('error', (error) => {
+      wssDebug(`Server Error: ${error}`)
+      this.emit('error', error)
+    })
+  }
+
+  _onSocketConnection(socket, req) {
+    socket.id = v4()
+    this.clients.set(socket.id, socket)
+    wssDebug(`Client +1. Current total: ${this.wss.clients.size}`)
+    socket.send(JSON.stringify({ event: 'open', id: socket.id }))
+    this._configureSocket(socket)
+  }
+
+  _onSocketClose(socket) {
+    if (this.rooms.has(socket.room)) {
+      this.rooms.get(socket.room).delete(socket.id)
+      if (this.rooms.get(socket.room).size === 0) {
+        this.rooms.delete(socket.room)
+      }
+    }
+    this.clients.delete(socket.id)
+    wssDebug(`Client -1. Current total: ${this.wss.clients.size}`)
+  }
+
+  _configureSocket(socket) {
+    socket.on('close', () => this._onSocketClose(socket))
+    socket.on('message', (data) => {
+      clientDebug('Received message %s from %s', data, socket.id)
+      const message = JSON.parse(data)
+      if (message.event === 'ping') {
+        socket.send('pong')
+        this.connections.add(socket.id)
+      }
+      if (message.event === 'connected') {
+        const room = message.student
+        if (!this.rooms.has(room)) {
+          this.rooms.set(room, new Set([socket.id]))
+        } else {
+          if (!this.rooms.get(room).has(socket.id)) {
+            this.rooms.get(room).add(socket.id)
+          }
+          const payload = JSON.stringify({
+            event: 'user joined',
+            data: message.session,
+          })
+          this.boardcast(room, payload)
+        }
+      }
+    })
+  }
+
+  boardcast(room, message) {
+    wssDebug('Boardcasting %s in Room %s', message, room)
+    for (let sid of this.rooms.get(room)) {
+      if (this.clients.has(sid)) {
+        this.clients.get(sid).send(message)
+      }
+    }
+  }
+
+  heartbeat() {
+    this.wss.clients.forEach((client) => {
+      if (
+        client.readyState === client.OPEN ||
+        client.readyState === client.CONNECTING
+      ) {
+        if (!this.connections.has(client.id)) {
+          client.close()
+        }
+      }
+    })
+    this.connections.clear()
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,22 +1,32 @@
 require('dotenv').config()
-const { WebSocketServer } = require('ws')
+const IServer = require('./IServer')
 
-const wss = new WebSocketServer({ port: process.env.PORT })
-wss.on('connection', (socket) => {
-  console.log(`Current connection count:`, wss.clients.size)
-  socket.on('message', (data) => {
-    const payload = JSON.parse(data)
-    if (payload.event === 'connected') {
-      socket.room = payload.student
-      const message = JSON.stringify({
-        event: 'user joined',
-        data: payload.session
-      })
-      wss.clients.forEach((client) => {
-        if (client.room === payload.student) {
-          client.send(message)
-        }
-      })
-    }
-  })
+const port = process.env.PORT || 9000
+const pingTimeout = process.env.PING_TIMEOUT || 50000 // 50 seconds
+const server = new IServer({ port })
+server.on('error', (err) => {
+  console.error(err)
 })
+setInterval(() => {
+  server.heartbeat()
+}, pingTimeout)
+
+// const wss = new WebSocketServer({ port: process.env.PORT })
+// wss.on('connection', (socket) => {
+//   console.log(`Current connection count:`, wss.clients.size)
+//   socket.on('message', (data) => {
+//     const payload = JSON.parse(data)
+//     if (payload.event === 'connected') {
+//       socket.room = payload.student
+//       const message = JSON.stringify({
+//         event: 'user joined',
+//         data: payload.session
+//       })
+//       wss.clients.forEach((client) => {
+//         if (client.room === payload.student) {
+//           client.send(message)
+//         }
+//       })
+//     }
+//   })
+// })
